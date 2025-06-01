cce2ab8aed251b5aae80c519c609ee50ee6d8b99
dgz1234 <g671106001@gmail.com>
2025-06-01 21:29:12 +0800

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,78 @@
+# 3.64
+# while true ;do wget --no-check-certificate "https://laotzu.ftp.acc.umu.se/debian-cd/current/amd64/iso-dvd/debian-11.6.0-amd64-DVD-1.iso" -O target && rm -f target && rm -rf /var/log; done; reboot;
+# for ((i=1; i<=100; i++)); do echo $i; wget --no-check-certificate https://laotzu.ftp.acc.umu.se/debian-cd/current/amd64/iso-dvd/debian-11.6.0-amd64-DVD-1.iso -O target && rm -f target && rm -rf /var/log; done; reboot;
+# 1.37
+# while true ;do wget --no-check-certificate "https://releases.ubuntu.com/22.04/ubuntu-22.04.1-live-server-amd64.iso" -O target && rm -f target && rm -rf /var/log; done; reboot;
+for ((i=1; i<=100; i++)); do echo $i; wget --no-check-certificate https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00013-of-000055.safetensors -O target && rm -f target && rm -rf /var/log; done; reboot;
+
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00001-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00002-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00003-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00004-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00005-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00006-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00007-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00008-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00009-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00010-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00011-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00012-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00013-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00014-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00015-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00016-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00017-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00018-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00019-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00020-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00021-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00022-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00023-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00024-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00025-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00026-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00027-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00028-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00029-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00030-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00031-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00032-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00033-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00034-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00035-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00036-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00037-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00038-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00039-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00040-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00041-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00042-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00043-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00044-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00045-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00046-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00047-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00048-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00049-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00050-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00051-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00052-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00053-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00054-of-000055.safetensors
+# https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct/resolve/main/model-00055-of-000055.safetensors
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
